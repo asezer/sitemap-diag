c900ba038e053c18b8fdf9d39b374796bc8effd7
asezer <anilsezer.com@gmail.com>
2015-05-26 17:52:17 -0400

    Tabs changed to 4 space

--- sitemap_diag.py
+++ sitemap_diag.py
@@ -3,175 +3,175 @@
 
 Usage:
 
-	python3 sitemap_diag.py http://www.example.com/sitemap.xml
+    python3 sitemap_diag.py http://www.example.com/sitemap.xml
 """
 import sys
 import requests
 from bs4 import BeautifulSoup
 try:
-	from urllib import parse as urlparse
+    from urllib import parse as urlparse
 except ImportError:
-	# Python 2
-	import urlparse
+    # Python 2
+    import urlparse
 
 def main(sitemapUrl):
-	"""Main module execution logic
+    """Main module execution logic
 
-	Args:
-		sitemap.xml Url
-	"""
-	sitemap_url = extract_url(sitemapUrl)
-	sitemap = sitemap_fetch(sitemap_url)
+    Args:
+        sitemap.xml Url
+    """
+    sitemap_url = extract_url(sitemapUrl)
+    sitemap = sitemap_fetch(sitemap_url)
 
-	sitemapLocations = fetch_locations_from_sitemap(sitemap)
+    sitemapLocations = fetch_locations_from_sitemap(sitemap)
 
-	if len(sitemapLocations) <= 0:
-		print ("No location found in the sitemap!")
-		sys.exit()
+    if len(sitemapLocations) <= 0:
+        print ("No location found in the sitemap!")
+        sys.exit()
 
-	problematicLocations = [] #List of problems
+    problematicLocations = [] #List of problems
 
-	sitemapLocations, problematicLocations = sitemap_check_dublicate(sitemapLocations)
-	problematicLocations += sitemap_check_accessibility(sitemapLocations)
+    sitemapLocations, problematicLocations = sitemap_check_dublicate(sitemapLocations)
+    problematicLocations += sitemap_check_accessibility(sitemapLocations)
 
-	print_sitemap_issues(problematicLocations, sitemap_url)
+    print_sitemap_issues(problematicLocations, sitemap_url)
 
 
 def extract_url(url):
-	"""Format the Url to http://example.com/sitemap.xml
+    """Format the Url to http://example.com/sitemap.xml
 
-	Args:
-		Given sitemap url from console
+    Args:
+        Given sitemap url from console
 
-	Returns:
-		Formated sitemap url
-	"""
-	parts = urlparse.urlsplit(url)
+    Returns:
+        Formated sitemap url
+    """
+    parts = urlparse.urlsplit(url)
 
-	if parts.scheme == '':
-		scheme = 'http'
-	else:
-		scheme = parts.scheme
+    if parts.scheme == '':
+        scheme = 'http'
+    else:
+        scheme = parts.scheme
 
-	urlpath = parts.path.split('/')
-	if parts.netloc == '':
-		netloc = urlpath[0]
-	else:
-		netloc = parts.netloc
+    urlpath = parts.path.split('/')
+    if parts.netloc == '':
+        netloc = urlpath[0]
+    else:
+        netloc = parts.netloc
 
-	if len(urlpath) == 1:
-		path = "sitemap.xml"
-	else:
-		path = urlpath[1]
+    if len(urlpath) == 1:
+        path = "sitemap.xml"
+    else:
+        path = urlpath[1]
 
-	needed_parts = urlparse.SplitResult(scheme=scheme, netloc=netloc, path=path, 
-	    query='', fragment='')
+    needed_parts = urlparse.SplitResult(scheme=scheme, netloc=netloc, path=path, 
+        query='', fragment='')
 
-	return needed_parts.geturl()
+    return needed_parts.geturl()
 
 
 def sitemap_fetch(sitemapUrl):
-	"""Fetch sitemap from given URL.
+    """Fetch sitemap from given URL.
 
-	Args:
-		sitemapUrl: The URL of the sitemap.
+    Args:
+        sitemapUrl: The URL of the sitemap.
 
-	Returns:
-		Fetched sitemap.xml.
-	"""
-	try:
-		r = requests.get(sitemapUrl)
-		soup = BeautifulSoup(r.text)
+    Returns:
+        Fetched sitemap.xml.
+    """
+    try:
+        r = requests.get(sitemapUrl)
+        soup = BeautifulSoup(r.text)
 
-		return soup
-	except:
-		print("Cannot fetch sitemap ", sitemapUrl)
-		sys.exit()
+        return soup
+    except:
+        print("Cannot fetch sitemap ", sitemapUrl)
+        sys.exit()
 
 
 def fetch_locations_from_sitemap(sitemap):
-	"""Fetch locations from given sitemap.xml
+    """Fetch locations from given sitemap.xml
 
-	Args:
-		sitemap.xml
+    Args:
+        sitemap.xml
 
-	Returns:
-		Fetched sitemap locations <loc> xml nodes
-	"""	
-	sitemapLocations = []
+    Returns:
+        Fetched sitemap locations <loc> xml nodes
+    """ 
+    sitemapLocations = []
 
-	for loc in sitemap.find_all('loc'):
-		sitemapLocations.append(str(loc.text))
+    for loc in sitemap.find_all('loc'):
+        sitemapLocations.append(str(loc.text))
 
-	return sitemapLocations
+    return sitemapLocations
 
 
 def sitemap_check_dublicate(sitemapLocations):
-	"""Finds dublicated locations in the given sitemap location list
+    """Finds dublicated locations in the given sitemap location list
 
-	Args:
-		sitemapLocations: List of sitemap locations
+    Args:
+        sitemapLocations: List of sitemap locations
 
-	Returns:
-		Sorted unique locations to check accessiblity
-		problematicLocations: Problematic location list to append
-	"""
-	problematicLocations = []
-	uniq = set()
-	for loc in sitemapLocations:
-		if loc not in uniq:
-			uniq.add(loc)
-		else:
-			problematicLocations.append("Dublicated Url: {}".format(loc))
+    Returns:
+        Sorted unique locations to check accessiblity
+        problematicLocations: Problematic location list to append
+    """
+    problematicLocations = []
+    uniq = set()
+    for loc in sitemapLocations:
+        if loc not in uniq:
+            uniq.add(loc)
+        else:
+            problematicLocations.append("Dublicated Url: {}".format(loc))
 
-	return sorted(uniq), problematicLocations
+    return sorted(uniq), problematicLocations
 
 
 def sitemap_check_accessibility(locations):
-	"""Hits every given URL and gets HTTP status Code
+    """Hits every given URL and gets HTTP status Code
 
-	Args:
-		List of URLs to test
+    Args:
+        List of URLs to test
 
-	Returns:
-		List of URL doesn't return 200 HTTP status code
-	"""
-	problematicLocations = []
-	for loc in locations:
-		print (loc)
-		r = requests.head(loc)
+    Returns:
+        List of URL doesn't return 200 HTTP status code
+    """
+    problematicLocations = []
+    for loc in locations:
+        print (loc)
+        r = requests.head(loc)
 
-		if r.status_code != 200:
-			if r.status_code == 404: #maybe the URL doesn't response to HEAD request
-				tmp = requests.get(loc) #let's try GET request
-				if tmp.status_code != 200:
-					problematicLocations.append("Status Code: {0} Url:{1}".format(r.status_code, loc))
-			else:
-				problematicLocations.append("Status Code: {0} Url:{1}".format(r.status_code, loc))
+        if r.status_code != 200:
+            if r.status_code == 404: #maybe the URL doesn't response to HEAD request
+                tmp = requests.get(loc) #let's try GET request
+                if tmp.status_code != 200:
+                    problematicLocations.append("Status Code: {0} Url:{1}".format(r.status_code, loc))
+            else:
+                problematicLocations.append("Status Code: {0} Url:{1}".format(r.status_code, loc))
 
-	return problematicLocations
+    return problematicLocations
 
 
 def print_sitemap_issues(problematicLocations, domainname):
-	"""Prints tested URLs which don't return 200
+    """Prints tested URLs which don't return 200
 
-	Args:
-		Iterable URLs which were tested and don't return 200
-	"""
-	if len(problematicLocations) == 0:
-		print("Good Job! There is no problem on your sitemap")
-		return
+    Args:
+        Iterable URLs which were tested and don't return 200
+    """
+    if len(problematicLocations) == 0:
+        print("Good Job! There is no problem on your sitemap")
+        return
 
-	print("\n~~List of problems~~")
-	with open(domainname + "-sitemap-problems.txt", "w") as f:
-		for problem in problematicLocations:
-			print (problem)
-			f.write(problem + "\n")
+    print("\n~~List of problems~~")
+    with open(domainname + "-sitemap-problems.txt", "w") as f:
+        for problem in problematicLocations:
+            print (problem)
+            f.write(problem + "\n")
 
 
 if __name__ == '__main__':
-	if len(sys.argv) == 1: # The 0th arg is the module filename
-		print ('Format should be "python3 sitemap_diag.py [Sitemap URL]"')
-		sys.exit()
+    if len(sys.argv) == 1: # The 0th arg is the module filename
+        print ('Format should be "python3 sitemap_diag.py [Sitemap URL]"')
+        sys.exit()
 
-	main(sys.argv[1])
+    main(sys.argv[1])
